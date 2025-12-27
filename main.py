9776035b40f422ec7a6cc38bd3519df528403234
Taylor Lougheed <taylor.lougheed@gmail.com>
2025-12-27 15:36:47 -0700

    Added triangle to screen. Updated main.py to use window size that hyprland is forcing.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    width, height = screen.get_size()
+
     clock = pygame.time.Clock()
     dt = 0
+    player = Player((width / 2), (height / 2))
 
     while True:
         log_state()
@@ -14,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) % 1000
 
