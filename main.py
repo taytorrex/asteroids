c10b8b7998147920fbc193077988b5e0a553e77a
Taylor Lougheed <taylor.lougheed@gmail.com>
2025-12-27 15:09:16 -0700

    Continued lesson

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("pygame size:", screen.get_size())
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,7 +15,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) % 1000
 
 if __name__ == "__main__":
     main()
