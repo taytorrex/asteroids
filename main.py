69899aee6244ab4c43b28cf216871db5505edb79
Taylor Lougheed <taylor.lougheed@gmail.com>
2025-12-28 14:30:01 -0700

    Added movement to triangle

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     width, height = screen.get_size()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     clock = pygame.time.Clock()
     dt = 0
     player = Player((width / 2), (height / 2))
-
+    
     while True:
         log_state()
+
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) % 1000
 
 if __name__ == "__main__":
     main()
